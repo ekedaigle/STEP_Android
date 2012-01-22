11d76722b668eec6f84d1de39fc9c1dd03eb3e0d
nimit <nimit@nimit-laptop>
2012-01-21 19:22:58 -0500

    Playing around with the newspaper template

--- STEP-Launcher/gen/com/step/launcher/R.java
+++ STEP-Launcher/gen/com/step/launcher/R.java
@@ -14,6 +14,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int button1=0x7f050007;
         public static final int email_button=0x7f050006;
         public static final int fragment_layout=0x7f050001;
         public static final int linearLayout1=0x7f050002;
