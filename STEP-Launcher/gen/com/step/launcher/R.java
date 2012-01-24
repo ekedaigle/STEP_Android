f006b3e841d604754d0af8baf87e2e8ec58db313
Eric Kern <eakmeister@gmail.com>
2012-01-24 18:45:34 -0500

    Created prettier buttons for the launcher.

--- STEP-Launcher/gen/com/step/launcher/R.java
+++ STEP-Launcher/gen/com/step/launcher/R.java
@@ -10,17 +10,26 @@ package com.step.launcher;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int button_height=0x7f040000;
+        public static final int button_spacing=0x7f040001;
+        public static final int button_text_size=0x7f040002;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int email_button=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int music_button=0x7f020002;
+        public static final int newspaper_button=0x7f020003;
+        public static final int store_button=0x7f020004;
     }
     public static final class id {
-        public static final int email_button=0x7f050006;
-        public static final int fragment_layout=0x7f050001;
-        public static final int linearLayout1=0x7f050002;
-        public static final int music_button=0x7f050003;
-        public static final int newspaper_button=0x7f050004;
-        public static final int store_button=0x7f050005;
-        public static final int text=0x7f050000;
+        public static final int email_button=0x7f060006;
+        public static final int fragment_layout=0x7f060001;
+        public static final int linearLayout1=0x7f060002;
+        public static final int music_button=0x7f060003;
+        public static final int newspaper_button=0x7f060004;
+        public static final int store_button=0x7f060005;
+        public static final int text=0x7f060000;
     }
     public static final class layout {
         public static final int email_fragment=0x7f030000;
@@ -30,11 +39,11 @@ public final class R {
         public static final int store_fragment=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int email_button=0x7f040005;
-        public static final int hello=0x7f040000;
-        public static final int music_button=0x7f040002;
-        public static final int newspaper_button=0x7f040003;
-        public static final int store_button=0x7f040004;
+        public static final int app_name=0x7f050001;
+        public static final int email_button=0x7f050005;
+        public static final int hello=0x7f050000;
+        public static final int music_button=0x7f050002;
+        public static final int newspaper_button=0x7f050003;
+        public static final int store_button=0x7f050004;
     }
 }
