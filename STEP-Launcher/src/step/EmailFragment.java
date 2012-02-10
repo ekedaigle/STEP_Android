f52727aa94722548f4777c34281cdff507a03889
Eric Kern <eakmeister@gmail.com>
2012-02-10 13:56:20 -0500

    Removed test JavaMail code

--- STEP-Launcher/src/step/EmailFragment.java
+++ STEP-Launcher/src/step/EmailFragment.java
@@ -1,16 +1,20 @@
 package step;
 
+import java.util.Properties;
+
 import com.step.launcher.R;
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class EmailFragment extends Fragment {
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-        return inflater.inflate(R.layout.email_fragment, container, false);
+		return inflater.inflate(R.layout.email_fragment, container, false);
 	}
 }
