04b09917ffce6a8630bb825a505328aea8dbb44b
Christopher Driscoll <driscoll.christopher@gmail.com>
2012-03-19 20:35:05 -0400

    Retool message layout into list. Added scrolling capability for reading messages.

--- STEP-Launcher/src/step/email/UpdateInboxTask.java
+++ STEP-Launcher/src/step/email/UpdateInboxTask.java
@@ -2,9 +2,9 @@ package step.email;
 
 import android.os.AsyncTask;
 
-public class UpdateInboxManually extends AsyncTask<String, Void, String>{
+public class UpdateInboxTask extends AsyncTask<String, Void, String>{
 	Mail m;
-	UpdateInboxManually(Mail m1){
+	UpdateInboxTask(Mail m1){
 		m = m1;
 	}
 	@Override
